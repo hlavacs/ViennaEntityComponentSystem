d60f830620ed58b32f52ce35144375d92134ccd1
hlavacs <helmut.hlavacs@univie.ac.at>
2021-05-03 09:12:53 +0200

    VecsTable

--- include/VECSTable.h
+++ include/VECSTable.h
@@ -35,9 +35,10 @@ namespace vecs {
 		static const size_t N = 1 << L;				///< Number if entries per segment
 		static const uint64_t BIT_MASK = N - 1;		///< Bit mask to mask off lower bits to get index inside segment
 
-		using tuple_value_t = vtll::to_tuple<DATA>;		///< Tuple holding the entries as value
-		using tuple_ref_t	= vtll::to_ref_tuple<DATA>;	///< Tuple holding references to the entries
-		using tuple_ptr_t	= vtll::to_ptr_tuple<DATA>;	///< Tuple holding references to the entries
+		using tuple_value_t = vtll::to_tuple<DATA>;			///< Tuple holding the entries as value
+		using tuple_ref_t	= vtll::to_ref_tuple<DATA>;		///< Tuple holding references to the entries
+		using tuple_rvref_t = vtll::to_rvref_tuple<DATA>;	///< Tuple holding references to the entries
+		using tuple_ptr_t	= vtll::to_ptr_tuple<DATA>;		///< Tuple holding references to the entries
 
 		using array_tuple_t1 = std::array<tuple_value_t, N>;								///< ROW: an array of tuples
 		using array_tuple_t2 = vtll::to_tuple<vtll::transform_size_t<DATA,std::array,N>>;	///< COLUMN: a tuple of arrays
@@ -65,16 +66,16 @@ namespace vecs {
 
 		template<typename TDATA>
 		requires std::is_same_v<TDATA, typename VecsTable<DATA, L, ROW>::tuple_value_t>
-		inline auto push_back(TDATA&& data) noexcept			-> table_index_t;	///< Push new component data to the end of the table
+		inline auto push_back(TDATA&& data) noexcept			-> table_index_t;		///< Push new component data to the end of the table
 
 		inline auto pop_back()	noexcept						-> void { m_size--; }	///< Remove the last row
 
 		template<size_t I, typename C = vtll::Nth_type<DATA, I>>
-		inline auto update(table_index_t n, C&& data) noexcept		-> bool;	///< Update a component  for a given row
+		inline auto update(table_index_t n, C&& data) noexcept	-> bool;				///< Update a component  for a given row
 
 		template<template<typename... Cs> typename T, typename... Cs>
 		requires std::is_same_v<vtll::to_tuple<DATA>, std::tuple<std::decay_t<Cs>...>>
-		inline auto update(table_index_t n, T<Cs...>&& data) noexcept -> bool;	///< Update components for a given row
+		inline auto update(table_index_t n, T<Cs...>&& data) noexcept		-> bool;	///< Update components for a given row
 
 		inline auto move(table_index_t idst, table_index_t isrc) noexcept	-> bool;	///< Move contents of a row to another row
 		inline auto swap(table_index_t n1, table_index_t n2) noexcept		-> bool;	///< Swap contents of two rows
@@ -189,16 +190,15 @@ namespace vecs {
 	template<typename TDATA>
 	requires std::is_same_v<TDATA, typename VecsTable<DATA, L, ROW>::tuple_value_t>
 	inline auto VecsTable<DATA, L, ROW>::push_back(TDATA&& data) noexcept -> table_index_t {
-		auto idx = m_size + 1;		///< Increase table size and get old size.
-		if (!reserve(idx + 1)) {	///< Make sure there is enough space in the table
+		auto idx = m_size.fetch_add(1);		///< Increase table size and get old size.
+		if (!reserve(idx + 1)) {			///< Make sure there is enough space in the table
 			m_size--;
-			return table_index_t{};		///< If not return an invalid index.
+			return table_index_t{};			///< If not return an invalid index.
 		}
 		decltype(auto) ref = tuple_ref(table_index_t{ idx });				///< Get references to components
 		vtll::static_for<size_t, 0, vtll::size<DATA>::value >([&](auto i) {
 			std::get<i>(ref) = std::get<i>(data);							///< Copy/move data over components
 			});
-		m_size++;															///< Publish new entry
 		return table_index_t{ static_cast<decltype(table_index_t::value)>(idx) };		///< Return new index
 	}
 
