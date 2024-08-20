e890129f72dafd5c170794ef468fe1d900475875
hlavacs <helmut.hlavacs@univie.ac.at>
2024-08-20 09:47:54 +0200

    archetype

--- include/VECS.h
+++ include/VECS.h
@@ -67,19 +67,22 @@ namespace vecs
 	
 		//----------------------------------------------------------------------------------------------
 
-		struct ComponentMapBase {
+		class ComponentMapBase {
+		public:
 			~ComponentMapBase() = default;
 			virtual std::size_t insert(Handle handle, std::any v) = 0;
 			virtual std::any get(std::size_t index) = 0;
 			virtual Handle erase(std::size_t index) = 0;
 			virtual std::any data() = 0;
 			virtual void move(std::any from, std::any to) = 0;
+			virtual size_t size() = 0;
 			virtual std::unique_ptr<ComponentMapBase> create() = 0;
 		};
 
 		template<typename T>
-		struct ComponentMap : ComponentMapBase {
-			std::vector<std::pair<Handle, T>> m_data;
+		class ComponentMap : ComponentMapBase {
+
+		public:
 
 			virtual std::size_t insert(Handle handle, std::any v) {
 				m_data.push_back(std::make_pair(handle, std::any_cast<T>(v)));
@@ -117,14 +120,20 @@ namespace vecs
 			virtual std::unique_ptr<ComponentMapBase> create() {
 				return std::make_unique<ComponentMap<T>>();
 			};
+
+			size_t size() {
+				return m_data.size();
+			}
+
+		private:
+			std::vector<std::pair<Handle, T>> m_data;
 		};
 
 		//----------------------------------------------------------------------------------------------
 
-		struct Archetype {
-			std::vector<std::type_index> m_types;	//must be sorted
-			std::unordered_map<std::type_index, std::unique_ptr<ComponentMapBase>> m_maps;
+		class Archetype {
 
+		public:
 			template<typename... Ts>
 				requires (vtll::unique<vtll::tl<Ts...>>::value)
 			Archetype() {
@@ -136,10 +145,10 @@ namespace vecs
 				(func.template operator()<Ts>(), ...);
 			}
 
-			template<typename T, bool ADD>
-			Archetype(const Archetype& other) {
+			template<typename T>
+			Archetype(const Archetype& other, size_t index, T* value) {
 				m_types = other.m_types;
-				if constexpr (ADD) {
+				if (value) {
 					assert(std::find(m_types.begin(), m_types.end(), type<T>()) == m_types.end());
 					m_types.push_back(type<T>());
 					m_maps[type<T>()] = std::make_unique<ComponentMap<T>>();
@@ -150,7 +159,7 @@ namespace vecs
 				}
 
 				for( auto& it : other.m_maps ) {
-					if constexpr (!ADD) {
+					if (value) {
 						if( it.first == type<T>() ) {
 							continue;
 						}
@@ -159,12 +168,69 @@ namespace vecs
 				}
 			}
 
-			template<typename T>
-			auto map() -> ComponentMap<T>* {
-				assert( m_maps.find(type<T>()) !=  m_maps.end() );
-				return static_cast<ComponentMap<T>*>(m_maps[type<T>()].get());
+			const auto& types() {
+				return m_types;
 			}
+
+			bool has(std::type_index ti) {
+				return (std::find(m_types.begin(), m_types.end(), ti) != std::end(m_types));
+			}
+
+			template<typename... Ts>
+			requires ((sizeof...(Ts) > 0) && (vtll::unique<vtll::tl<Ts...>>::value))
+			[[nodiscard]]
+			auto create( Handle handle, Ts&&... component ) -> size_t {
+				size_t index{0};
+				bool first{true};
+				auto func = [&](auto&& v) {
+					size_t nindex = it.second->map<std::decay_t<decltype(v)>>.insert(handle, v);
+					assert(first || index == nindex);
+					index = nindex;
+					first = false;
+				;
+				(func(component), ...);
+				return index;
+			}
+
+			template<typename T>
+			[[nodiscard]]
+			T get(std::size_t index) {
+				auto it = std::find(m_types.begin(), m_types.end(), ti);
+				assert(it != std::end(m_types) && index < m_maps[*it]->size());
+				return std::any_cast<T>(m_maps[*it]->get(index));
+			}
+
+			Handle erase(std::size_t index) {
+				
+			}
+
+			template<typename T>
+			auto data() -> std::vector<std::pair<Handle, T>>* {
+				auto it = m_maps.find(type<T>());
+				assert(it != m_maps.end());
+				return std::any_cast<std::vector<std::pair<Handle, T>>*>(it->second->data());
+			}
+
+		private:
+
+			template<typename T>
+			auto ptr(Handle handle) -> T* {
+				auto it = m_entities.find(handle);
+				assert(it != m_entities.end());
+				auto& [arch, index] = it->second;
+
+				if( arch->m_maps.find(type<T>()) == arch->m_maps.end() ) {
+					arch->m_maps[type<T>()] = std::make_unique<ComponentMap<T>>();
+
+				}
+
+				return &(std::any_cast<std::pair<Handle,T>*>(m_entities[type<T>()]->get(handle)))->second;
+			}
+
+			std::vector<std::type_index> m_types;
+			std::unordered_map<std::type_index, std::unique_ptr<ComponentMapBase>> m_maps;
 		};
+
 
 		struct ArchetypeIndex {
 			Archetype* m_archetype;
@@ -199,13 +265,8 @@ namespace vecs
 				it = m_archetypes.find(&types);
 			}
 
-			size_t index{0};
-			auto func = [&](Handle handle, auto&& v) {
-				size_t nindex = it.second->map<std::decay_t<decltype(v)>>.insert(handle, v);
-				assert(index == 0 || index == nindex);
-				index = nindex;
-			};
-			(func(handle, component), ...);
+			auto index = it->second->create(handle, std::forward<Ts>(component)...);
+
 			m_entities[handle] = { it, index };
 			return handle;
 		}
