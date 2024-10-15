09564d0613179fbb791efeb1fd6d5c2fe7f17193
hlavacs <helmut.hlavacs@univie.ac.at>
2024-10-15 11:32:19 +0200

    DartConfiguration

--- build/DartConfiguration.tcl
+++ build/DartConfiguration.tcl
@@ -63,8 +63,8 @@ UpdateOptions:
 UpdateType: git
 
 # Compiler info
-Compiler: C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.40.33807/bin/Hostx64/x64/cl.exe
-CompilerVersion: 19.40.33808.0
+Compiler: C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang-cl.exe
+CompilerVersion: 17.0.3
 
 # Dynamic analysis (MemCheck)
 PurifyCommand: 
