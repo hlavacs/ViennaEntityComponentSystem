f592e854bd4dc6f5fc1f2f4c1c5ecabf93332efe
MarleneKLucy <96651662+MarleneKLucy@users.noreply.github.com>
2025-04-30 14:08:49 +0200

    code cleanup

--- build.alt/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/001_Uni/001_2025_BACHELORARBEIT/VECS/ViennaEntityComponentSystem
-# Build directory: D:/001_Uni/001_2025_BACHELORARBEIT/VECS/ViennaEntityComponentSystem/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/viennastrongtype-build")
-subdirs("_deps/viennatypelistlibrary-build")
-subdirs("tests")
--- build.alt/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/001_Uni/001_2025_BACHELORARBEIT/VECS/ViennaEntityComponentSystem/tests
-# Build directory: D:/001_Uni/001_2025_BACHELORARBEIT/VECS/ViennaEntityComponentSystem/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
