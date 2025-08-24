0254bc113a403f501215ab83d6265f49821c770e
Isabella Hölzl <hoeli90@outlook.com>
2025-08-24 20:22:23 +0200

    added structs for more complex entity creation function

--- tests/test_helper.cpp
+++ tests/test_helper.cpp
@@ -1,4 +1,5 @@
 #include "VECSManager.h"
+#include "test_structs.cpp"
 
 /// @brief Inserts entities of one, two or three components of int, double and float
 size_t fillRegistryBasic(vecs::Manager& mng, int oneComp = 5, int twoComp = 5, int threeComp = 5) {
@@ -24,6 +25,38 @@ size_t fillRegistryBasic(vecs::Manager& mng, int oneComp = 5, int twoComp = 5, i
     for (size_t i = 0; i < threeComp; ++i) {
         mng.Insert(integers[i], doubles[i], floats[i]);
         totalNew += 1;
+    }
+
+    return totalNew;
+}
+
+
+
+
+/// @brief Inserts entities of one, two or three components of sprite, position and velocity
+size_t fillRegistryComplex(vecs::Manager& mng, int oneComp = 5, int twoComp = 5, int threeComp = 5) {
+    std::vector<Sprite> sprites = {{'a'}, {'b'}, {'c'}, {'d'}, {'e'}};
+    std::vector<Position> positions = {{1.0f, 1.0f}, {2.0f, 2.0f}, {1.2f, 2.3f}, {3.4f, 4.5f}, {5.6f, 6.7f}};
+    std::vector<Velocity> velocities = {{1.0f, 1.5f}, {1.5f, 1.5f}, {1.1f, 2.2f}, {2.0f, 1.0f}, {3.5f, 3.0f}};
+
+    size_t totalNew = 0;
+
+    for (size_t i = 0; i < oneComp; ++i) {
+        mng.Insert(sprites[i]);
+        totalNew += 1;
+    }
+
+    for (size_t i = 0; i < twoComp; ++i) {
+        mng.Insert(sprites[i], positions[i]);
+        mng.Insert(sprites[(i+1)%sprites.size()], positions[i]);
+        mng.Insert(positions[(i+1)%positions.size()], velocities[(i+1)%velocities.size()]);
+        totalNew += 3;
+    }
+
+    for (size_t i = 0; i < threeComp; ++i) {
+        mng.Insert(sprites[i], positions[i], velocities[i]);
+        mng.Insert(sprites[(i+1)%sprites.size()], positions[i], velocities[(i+1)%velocities.size()]);
+        totalNew += 2;
     }
 
     return totalNew;
---
+++ tests/test_structs.cpp
@@ -0,0 +1,13 @@
+struct Position {
+    float x{0.0f};
+    float y{0.0f};
+};
+
+struct Velocity {
+    float x{1.0f};
+    float y{1.0f};
+};
+
+struct Sprite {
+    char name{' '};
+};
