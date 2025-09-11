d54e144b9ce6de65350c1a3a727ef13bc90b5876
Isabella Hölzl <hoeli90@outlook.com>
2025-09-11 17:43:36 +0200

    Copy Constructor for Position and Velocity

--- tests/test_structs.cpp
+++ tests/test_structs.cpp
@@ -1,11 +1,29 @@
 struct Position {
     float x{0.0f};
     float y{0.0f};
+
+    Position() = default;
+    Position(float a, float b) {
+        x = a;
+        y = b;
+    }
+    Position(const Position& pos) : Position(pos.x, pos.y) {}
+
+    bool operator==(const Position& rp) const {
+        return (x == rp.x && y == rp.y);
+    }
 };
 
 struct Velocity {
     float x{1.0f};
     float y{1.0f};
+
+    Velocity() = default;
+    Velocity(float a, float b) {
+        x = a;
+        y = b;
+    }
+    Velocity(const Velocity& vel) : Velocity(vel.x, vel.y) {}
 };
 
 struct Sprite {
